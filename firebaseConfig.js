cb8df3dd09779e90eaa2ce430305b83507f5e7b3
ronak-gami <ronakg.itpath@gmail.com>
2025-05-13 10:27:49 +0530

    firebaseConfig added

---
+++ firebaseConfig.js
@@ -0,0 +1,9 @@
+export const firebaseConfig = {
+  apiKey: 'AIzaSyA2QvwVks8JyIvtpf6B3dFlFo3VNoeZkTA',
+  authDomain: 'demoit-723c4.firebaseapp.com',
+  projectId: 'demoit-723c4',
+  storageBucket: 'demoit-723c4.firebasestorage.app',
+  messagingSenderId: '522112965987',
+  appId: '1:522112965987:web:664481a5d76339a18c8756',
+  measurementId: 'G-VZ1WBCSWTZ',
+};
