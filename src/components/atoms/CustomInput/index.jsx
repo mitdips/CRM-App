667035d645b76def59b767abe84fc41a91657730
purvin_itpath <purvin.itpath@gmail.com>
2025-05-13 09:50:33 +0530

    folderStructure Added

---
+++ src/components/atoms/CustomInput/index.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import { StyleSheet, View } from 'react-native';
+
+const Input = () => {
+    return (
+        <View>
+            
+        </View>
+    );
+}
+
+const styles = StyleSheet.create({})
+
+export default Input;
---
+++ src/components/atoms/CustomText/index.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import { StyleSheet, View } from 'react-native';
+
+const Text = () => {
+    return (
+        <View>
+            
+        </View>
+    );
+}
+
+const styles = StyleSheet.create({})
+
+export default Text;
