06bd5986f30ea3a620d20d9a7269f6a50e08ce94
ronak-gami <ronakg.itpath@gmail.com>
2025-05-14 15:48:47 +0530

    Template edit

--- src/components/templates/AuthTemplate/index.jsx
+++ src/components/templates/AuthTemplate/index.jsx
@@ -2,12 +2,12 @@ import {View} from 'react-native';
 import {useStyle} from './style';
 import LogoText from '../../molecules/LogoText';
 const styles = useStyle();
-
+ 
 const AuthTemplate = ({children}) => {
   return (
     <View style={styles.container}>
       <LogoText />
-      {children}
+      <View style={styles.innerContainer}>{children}</View>
     </View>
   );
 };
