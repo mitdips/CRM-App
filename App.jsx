652ea320e8e0fde215ba6f3482c97ecae6581d61
ronak-gami <ronakg.itpath@gmail.com>
2025-05-15 18:17:25 +0530

    Splash Screen by PN

--- App.jsx
+++ App.jsx
@@ -1,8 +1,16 @@
-import {View} from 'react-native';
+import {Platform, View} from 'react-native';
 import Text from './src/components/atoms/Text';
-
+import { useEffect } from 'react';
+import SplashScreen from 'react-native-splash-screen'
 const App = () => {
+
   return (
+    useEffect(() => {
+    if (Platform.OS === 'android') {
+      SplashScreen.hide();
+    }
+  }
+  , []),
     <View style={{flex: 1, justifyContent: 'center', alignItems: 'center'}}>
       <Text>CRM App</Text>
     </View>
