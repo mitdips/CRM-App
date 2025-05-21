de7cc50823faba520e0a0ea325b63e5f7b117846
purvin_itpath <purvin.itpath@gmail.com>
2025-05-21 10:48:07 +0530

    Final Step of Splash Screen

--- App.jsx
+++ App.jsx
@@ -1,16 +1,12 @@
-import {Platform, View} from 'react-native';
+import {View} from 'react-native';
 import Text from './src/components/atoms/Text';
-import { useEffect } from 'react';
+import {useEffect} from 'react';
 import SplashScreen from 'react-native-splash-screen';
 const App = () => {
-
+  useEffect(() => {
+    SplashScreen.hide();
+  }, []);
   return (
-    useEffect(() => {
-    if (Platform.OS === 'android') {
-      SplashScreen.hide();
-    }
-  }
-  , []),
     <View style={{flex: 1, justifyContent: 'center', alignItems: 'center'}}>
       <Text>CRM App</Text>
     </View>
