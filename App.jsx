7b5b4f6426c31c387f3fbee46b840d3ea1ec8af6
purvin_itpath <purvin.itpath@gmail.com>
2025-05-21 13:41:59 +0530

    Integrate SplashScreen functionality in App component

--- App.jsx
+++ App.jsx
@@ -1,10 +1,11 @@
 import {useEffect} from 'react';
 import {GoogleSignin} from '@react-native-google-signin/google-signin';
 import StackNavigator from './src/navigations/StackNavigation';
+import SplashScreen from 'react-native-splash-screen';
 // import useTemporarySignOut from './useTemporarySignOut';
-
 const App = () => {
   useEffect(() => {
+    SplashScreen.hide();
     GoogleSignin.configure({
       webClientId:
         '522112965987-tldhg24l07j2eoqqe9jpdeo5phdt98ss.apps.googleusercontent.com',
